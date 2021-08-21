32abc6a0514651a0d9a8c6a991386e98274b7208
mithulix <mohaiminul49@gmail.com>
2021-08-21 22:51:16 +0600

    new updates

--- index.js
+++ index.js
@@ -4,23 +4,93 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 const port = 5000;
 
 //mongodb database require functions
-// const objectId = require('mongodb').objectId;
 const MongoClient = require('mongodb').MongoClient;
-const {objectId} = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sckbv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true });
+const  ObjectId = require('mongodb').ObjectId;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0xs55.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // db connection checking
-app.get("/", function (req, res) {res.send("Flash-bright database connected");})
+app.get("/", function (req, res) { res.send("Flash-bright database connected"); })
 
 //DB connection between client-server
 client.connect(err => {
-    const Pictures = client.db("flashBright").collection("pictures");
-})
+    //Database
+    const Services = client.db("flashBright").collection("services");
+    const Orders = client.db("flashBright").collection("orders");
+    const Admin = client.db("flashBright").collection("admin");
 
+    console.log('database connected');
+
+    // MainPage
+    //get services
+    app.get("/getServices", (req, res) => {
+        Services.find({})
+            .toArray((err, documents) => { res.send(documents) })
+    })
+
+    // get services by id
+    app.get('/getServices/:id', (req, res) => {
+        Services.find({ _id: ObjectId(req.params.id) })
+            .toArray((err, documents) => { res.send(documents[0]) })
+    })
+
+    // Booking panel
+    // new booking
+    app.post('/newBooking', (req, res) => {
+        Orders.insertOne(req.body)
+            .then(result => { res.send(result.insertedCount > 0) })
+    })
+
+    // show Booking
+    app.get('/getBookingList', (req, res) => {
+        Orders.find({ email: req.body.email })
+            .toArray((err, documents) => { res.send(documents) })
+    })
+
+
+    // Admin panel
+    // All Orders
+    app.get("/allOrders", (req, res) => {
+        Orders.find({})
+            .toArray((err, documents) => { res.send(documents) })
+    })
+
+    // Update orders
+    app.patch("/allOrders/:id", (req, res) => {
+        Orders.updateOne({ _id: ObjectId(req.params.id) },
+            { $set: { status: req.body.status } })
+            .then(result => { res.send(result.modifiedCount > 0) })
+    })
+
+    //Add services
+    app.post('/addService', (req, res) => {
+        Services.insertOne(req.body)
+            .then(result => { res.send(result.insertedCount > 0) })
+    });
+
+    //delete service
+    app.delete('/deleteService/:id', (req, res) => {
+        Services.deleteOne({ _id: ObjectId(req.params.id) })
+            .then(result => { res.send(result.deletedCount > 0) })
+    })
+
+    //makeAdmin
+    app.get('/makeAdmin', (req, res) => {
+        Admin.insertOne(req.body)
+            .then(result => { res.send(result.insertedCount > 0) })
+    })
+
+    // getAdmin
+    app.get('/getAdmin', (req, res) => {
+        Admin.findOne(req.params.id)
+            .toArray((err, documents) => { res.send(documents) })
+    })
+
+});
 
 app.listen(process.env.PORT || port)
